7b0236425c4f8c22485aee252c6e6e9adc4c4087
ptquang86 <ptquang86@gmail.com>
2015-11-20 10:08:50 +0700

    update alloy.js

--- app/alloy.js
+++ app/alloy.js
@@ -1,11 +1,26 @@
-// The contents of this file will be executed before any of
-// your view controllers are ever executed, including the index.
-// You have access to all functionality on the `Alloy` namespace.
-//
-// This is a great place to do any initialization for your app
-// or create any global variables/functions that you'd like to
-// make available throughout your app. You can easily make things
-// accessible globally by attaching them to the `Alloy.Globals`
-// object. For example:
-//
-// Alloy.Globals.someGlobalFunction = function(){};
+/*
+(function() {
+	
+	var displayCaps = Ti.Platform.displayCaps,
+		platformWidth = displayCaps.platformWidth,
+		platformHeight = displayCaps.platformHeight;
+	
+	Ti.API.info('ScreenSize: ' + JSON.stringify( [platformWidth, platformHeight] ));
+	
+	if (OS_ANDROID) {
+		var measurement = require('alloy/measurement');
+		platformWidth = Math.floor(measurement.pxToDP(platformWidth));
+		platformHeight = Math.floor(measurement.pxToDP(platformHeight));
+		Ti.API.info('ScreenSize: ' + JSON.stringify( [platformWidth, platformHeight] ));
+		Ti.API.info('if ( ScreenSize == ScreenSize scaled ) { Delete the [build] + [Resources] folders and build again. }'); 
+	}
+	
+  	Alloy.Globals.UI = {
+  		Win: {
+  			Width: platformWidth,
+  			Height: platformHeight
+  		}
+  	};
+  	
+})();
+*/
